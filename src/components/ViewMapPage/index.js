fcba2734f5accc786919bba2846c0fb9f2c3ae45
Dipesh Makwana <dipesh_makwana@hotmail.co.uk>
2021-04-04 14:54:46 +0100

    AutoComplete Field To Search (Template) Implemented

--- src/components/ViewMapPage/index.js
+++ src/components/ViewMapPage/index.js
@@ -1,4 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import ViewMapCanvas from '../ViewMapCanvas';
 import firebase from '../firebase';
@@ -11,21 +13,46 @@ const ViewMapPage = () => {
     useEffect(() => {
         var db = firebase.database();
         var data = db.ref("MapData/-MXCqzQ3DNVYTzfiFqxr");
-    
+
         data.on('value', (snapshot) => {
             const data = snapshot.val();
             setMapData([...data.mapData]);
             setCategories([...data.categories]);
         });
     }, [])
-    
+
 
     return (
-
-        <ViewMapCanvas shapes={mapData} categories={categories}/>
+        <React.Fragment>
+            {mapData &&
+                <Autocomplete
+                    style={{ width: 300 }}
+                    options={mapData.filter(function (shape) {
+                        return shape.name === "building";
+                    })}
+                    autoHighlight
+                    getOptionLabel={(option) => option.label}
+                    renderOption={(option) => (
+                        option.label
+                    )}
+                    renderInput={(params) => (
+                        <TextField
+                          {...params}
+                            label="Search For A Building"
+                            variant="outlined"
+                            inputProps={{
+                                ...params.inputProps,
+                                // autoComplete: 'new-password', // disable autocomplete and autofill
+                            }}
+                        />
+                    )}
+                />
+            }
+            <ViewMapCanvas shapes={mapData} categories={categories} />
+        </React.Fragment>
 
     );
-    
+
 }
 
 export default ViewMapPage;
