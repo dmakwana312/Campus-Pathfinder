4645302787a7ce8becc331648604556b51c850b9
Dipesh Makwana <dipesh_makwana@hotmail.co.uk>
2021-04-07 15:45:04 +0100

    Legend Updated

--- src/components/CategoryLegend/index.js
+++ src/components/CategoryLegend/index.js
@@ -5,18 +5,33 @@ import { useStyles } from '../style.js';
 
 const CategoryLegend = (props) => {
     const classes = useStyles();
+   
     return (
         <Paper className={classes.paper} style={{ width: 150, top: 0, right: 0 }} elevation={10}>
-            {props.categories.map((category, key) => {
-                return (
-                    <div>
-                        <div style={{ margin: 10, float: "left", border: "1px solid black", height: "10px", width: "10px", backgroundColor: category.mainColour }} />
-                        <p style={{paddingTop: 5 }}> {category.categoryName}</p>
+            <div>
+                {props.categories.map((category, key) => {
+                    return (
+                        <React.Fragment>
+                            <div style={{ margin: 10, float: "left", border: "1px solid black", height: "10px", width: "10px", backgroundColor: category.mainColour }} />
+                            <p style={{ paddingTop: 5 }}> {category.categoryName}</p>
+                        </React.Fragment>
 
-                    </div>
-                );
+                    );
 
-            })}
+                })}
+                <div style={{ margin: 10, float: "left", border: "1px solid black", height: "10px", width: "10px", backgroundColor: "#03b1fc" }} />
+                <p style={{ paddingTop: 5 }}> Search Result</p>
+
+                <div style={{ margin: 10, float: "left", border: "1px solid black", height: "10px", width: "10px", backgroundColor: "#03fc0f" }} />
+                <p style={{ paddingTop: 5 }}> Origin</p>
+
+                <div style={{ margin: 10, float: "left", border: "1px solid black", height: "10px", width: "10px", backgroundColor: "#fc03ce" }} />
+                <p style={{ paddingTop: 5 }}> Destination</p>
+
+                <div style={{ margin: 10, float: "left", border: "1px solid black", height: "10px", width: "10px", backgroundColor: "#0000FF" }} />
+                <p style={{ paddingTop: 5 }}> Route</p>
+
+            </div>
         </Paper>
     );
 }
