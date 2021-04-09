80a6fd22cf0a53a9571ac91cd710d779acf40b0c
Dipesh Makwana <dipesh_makwana@hotmail.co.uk>
2021-04-09 10:25:04 +0100

    Login/Registration System Implemented

--- src/components/LoginPaper/index.js
+++ src/components/LoginPaper/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
@@ -8,22 +8,87 @@ import Modal from '@material-ui/core/Modal';
 import { useStyles } from '../style.js';
 import { Button } from '@material-ui/core';
 
+import Firebase from '../../utils/firebase';
+
 const LoginPaper = () => {
 
     const classes = useStyles();
 
     const [showRegisterModal, setShowRegisterModal] = useState(false);
 
+    const [user, setUser] = useState(null);
     const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState("");
     const [password, setPassword] = useState("");
+    const [passwordError, setPasswordError] = useState("");
     const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [passwordConfirmError, setPasswordConfirmError] = useState("");
+
+    useEffect(() => {
+        setEmailError("");
+        setPasswordError("");
+        setPasswordConfirmError("");
+    }, [showRegisterModal])
+
+    useEffect(() =>{
+        authListener();
+    }, [])
 
     function login() {
-        console.log(email, password);
+        Firebase.auth()
+            .signInWithEmailAndPassword(email, password)
+            .catch(error => {
+                switch(error.code){
+                    case "auth/invalid-email":
+                    
+                    case "auth/user-disabled":
+
+                    case "auth/user-not-found":
+                        setEmailError(error.message);
+                        break;
+                    case "auth/wrong-password":
+                        setPasswordError(error.message);
+                    
+                }
+            });
+    }
+
+    function logout() {
+        Firebase.auth().signOut();
+    }
+
+    function authListener() {
+        Firebase.auth().onAuthStateChanged(user => {
+            if(user){
+                setUser(user);
+            }
+            else{
+                setUser("");
+            }
+        })
     }
 
     function register() {
-        console.log(email, password);
+        if (password === passwordConfirm) {
+            Firebase.auth()
+            .createUserWithEmailAndPassword(email, password)
+            .catch(error => {
+                switch(error.code){
+
+                    case "auth/email-already-in-use":
+                    case "auth/invalid-email":
+                        setEmailError(error.message);
+                        break;
+                    case "auth/weak-password":
+                        setPasswordError(error.message);
+                    
+                }
+            });
+        }
+        else {
+            setPasswordError("Passwords Do Not Match");
+            setPasswordConfirmError("Passwords Do Not Match");
+        }
     }
 
     return (
@@ -32,8 +97,8 @@ const LoginPaper = () => {
             <Paper className={classes.loginPaper} >
 
                 <form className={classes.form}>
-                    <TextField className={classes.formTextfield} id="emailLogin" label="Email" variant="outlined" onChange={(event) => setEmail(event.target.value)} />
-                    <TextField className={classes.formTextfield} id="passwordLogin" type="password" label="Password" variant="outlined" onChange={(event) => setPassword(event.target.value)} />
+                    <TextField error={emailError === "" ? false : true} helperText={ emailError } className={classes.formTextfield} id="emailLogin" label="Email" variant="outlined" onChange={(event) => setEmail(event.target.value)} />
+                    <TextField error={passwordError === "" ? false : true} helperText={ passwordError } className={classes.formTextfield} id="passwordLogin" type="password" label="Password" variant="outlined" onChange={(event) => setPassword(event.target.value)} />
 
                     <Button style={{ margin: 18, width: '30ch', marginBottom: 10 }} variant="contained" color="primary" onClick={login}>
                         Login
@@ -58,9 +123,9 @@ const LoginPaper = () => {
                     <Paper className={classes.loginPaper} >
 
                         <form className={classes.form}>
-                            <TextField className={classes.formTextfield} id="emailRegister" label="Email" variant="outlined" onChange={(event) => setEmail(event.target.value)} />
-                            <TextField className={classes.formTextfield} id="passwordRegister" type="password" label="Password" variant="outlined" onChange={(event) => setPassword(event.target.value)} />
-                            <TextField className={classes.formTextfield} id="passwordRegisterConfirm" type="password" label="Confirm Password" variant="outlined" onChange={(event) => setPasswordConfirm(event.target.value)} />
+                            <TextField error={emailError === "" ? false : true} helperText={ emailError } className={classes.formTextfield} id="emailRegister" label="Email" variant="outlined" onChange={(event) => setEmail(event.target.value)} />
+                            <TextField error={passwordError === "" ? false : true} helperText={ passwordError } className={classes.formTextfield} id="passwordRegister" type="password" label="Password" variant="outlined" onChange={(event) => setPassword(event.target.value)} />
+                            <TextField error={passwordConfirmError === "" ? false : true} helperText={ passwordConfirmError } className={classes.formTextfield} id="passwordRegisterConfirm" type="password" label="Confirm Password" variant="outlined" onChange={(event) => setPasswordConfirm(event.target.value)} />
 
 
                             <Button style={{ margin: 18, width: '30ch', marginBottom: 0 }} variant="contained" color="primary" onClick={register}>
